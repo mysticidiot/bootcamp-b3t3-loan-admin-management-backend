a02de5767005fd5d9b9703307577a5ff539718ff
Administrator <Administrator@172.20.0.30>
2022-11-03 17:09:01 +0530

    Initial Commit

---
+++ src/main/java/com/b3t3/loanAdminManagement/LoanAdminManagementApplication.java
@@ -0,0 +1,13 @@
+package com.b3t3.loanAdminManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanAdminManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanAdminManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/b3t3/loanAdminManagement/LoanAdminManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b3t3.loanAdminManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanAdminManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
