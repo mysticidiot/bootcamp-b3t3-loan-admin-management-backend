b5118b36814dc451e3efa9e30fdd0f25e463607b
unknown <hsamarth2000@gmail.com>
2022-11-08 21:50:37 +0530

    ds

--- src/main/java/com/b3t3/loanAdminManagement/Controller/CardController.java
+++ src/main/java/com/b3t3/loanAdminManagement/Controller/CardController.java
@@ -3,6 +3,7 @@ package com.b3t3.loanAdminManagement.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,22 +31,25 @@ public class CardController {
 	
 	@Autowired
 	Loan_Card_Master_service_impl card_service;
-	
+	@CrossOrigin(origins="http://localhost:3000")
 	@PostMapping("/add")
 	public String addCard(@RequestBody Loan_Card_Master card) {
 		return card_service.addCard(card);
 	}
-	
+	@CrossOrigin(origins="http://localhost:3000")
 	@GetMapping("/display")
 	public List<Loan_Card_Master> showCards(){
 		return (List<Loan_Card_Master>)card_service.displayAllCards();	
 	}
 	
+	@CrossOrigin(origins="http://localhost:3000")
 	@GetMapping("/delete/{id}")
-	public String deleteCard(@PathVariable String id){
-		return card_service.deleteCard(id);
+	public void deleteCard(@PathVariable String id){
+		//SSystem.out.print(id);
+		 card_service.deleteCard(id);
 	}
 	
+	@CrossOrigin(origins="http://localhost:3000")
 	@PostMapping("/edit")
 	public String updateCard(@RequestBody Loan_Card_Master update_card) {
 		return card_service.updateCard(update_card);
